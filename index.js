524ef888f6faea1372335933252d066e38d9ea56
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-09-28 17:00:26 +0300

    Fix conversion

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 function calcPercent(grade) {
-    const grades = [
-        [0.7, 0.99, 100, 100],
-        [1.0, 1.29, 94, 100],
-        [1.3, 1.69, 90, 93.9],
-        [1.7, 1.99, 86, 89.9],
-        [2.0, 2.29, 82, 85.9],
-        [2.3, 2.69, 78, 81.9],
-        [2.7, 2.99, 74, 77.9],
-        [3.0, 3.29, 70, 73.9],
-        [3.3, 3.69, 65, 69.9],
-        [3.7, 3.99, 60, 64.9],
-        [4.0, 4.99, 50, 59.9],
-        [5.0, 6.0, 0, 49.9]
-    ];
+  const grades = [
+    [0.7, 0.99, 100, 100],
+    [1.0, 1.29, 95, 100],
+    [1.3, 1.69, 87.5, 95],
+    [1.7, 1.99, 82.5, 87.5],
+    [2.0, 2.29, 77.5, 82.5],
+    [2.3, 2.69, 72.5, 77.5],
+    [2.7, 2.99, 67.5, 72.5],
+    [3.0, 3.29, 62.5, 67.5],
+    [3.3, 3.69, 57.5, 62.5],
+    [3.7, 3.99, 52.5, 57.5],
+    [4.0, 4.99, 50, 52.5],
+    [5.0, 6.0, 0, 50],
+  ];
 
-    for (let i = 0; i < grades.length; i++) {
-        const key = grades[i];
-        if (grade >= key[0] && grade <= key[1]) {
-            return (key[1] - grade) * (key[3] - key[2]) / (key[1] - key[0]) + key[2];
-        }
+  for (let i = 0; i < grades.length; i++) {
+    const key = grades[i];
+    if (grade >= key[0] && grade <= key[1]) {
+      return ((key[1] - grade) * (key[3] - key[2])) / (key[1] - key[0]) + key[2];
     }
-    return -1;
+  }
+  return -1;
 }
 
 function getGPA(percent) {
-    return Math.round(percent / 100 * 4 * 100) / 100;
+  return Math.round((percent / 100) * 4 * 100) / 100;
 }
 
 function calcGPA(gpa) {
-    if (gpa < 0.7 || gpa > 6.0) {
-        return "Invalid GPA";
-    }
-    const percent = calcPercent(gpa);
-    const americanGPA = getGPA(percent);
-    return "Your American GPA is: " + americanGPA;
+  if (gpa < 0.7 || gpa > 6.0) {
+    return 'Invalid GPA';
+  }
+  const percent = calcPercent(gpa);
+  const americanGPA = getGPA(percent);
+  return 'Your American GPA is: ' + americanGPA;
 }
 
 function convertGPA() {
-    const gucGPA = parseFloat(document.getElementById("guc-gpa").value);
-    const result = document.getElementById("result");
-    result.innerHTML = calcGPA(gucGPA);
+  const gucGPA = parseFloat(document.getElementById('guc-gpa').value);
+  const result = document.getElementById('result');
+  result.innerHTML = calcGPA(gucGPA);
 }
